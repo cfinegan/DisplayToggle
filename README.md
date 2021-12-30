0d47c5712848c57446d9ec16bc4523f9193d0f0f
cfinegan <conor_finegan@student.uml.edu>
2021-12-29 23:53:05 -0500

    Presentation modes can now be specified on the command line
    The program is no longer hard-coded to swap between 'internal' and 'extend' modes.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DisplayToggle
-Quick toggle between "PC Screen Only" and "Extend" presentation modes on Windows.
+
+Quickly toggle between any 2 specified presentation modes on Windows. Basically a better version of the `Win`+`P` shortcut for users who frequently swap modes.
+
+## Usage
+
+`DisplayToggle.exe <primary> <secondary>`
+
+*DisplayToggle* allows the user to specify a "primary" and "secondary" display mode on the command line. When the program is run, it will swap to the primary mode if the current mode is any other than the primary. It will swap to the secondary mode if the current mode is the primary.
+
+Argument strings map to display modes as follows:
+
+| argv         | `Win`+`P`          | [`DISPLAYCONFIG_TOPOLOGY_ID`](https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ne-wingdi-displayconfig_topology_id) |
+| ------------ | ------------------ | --------------------------------- |
+| `"internal"` | PC screen only     | `DISPLAYCONFIG_TOPOLOGY_INTERNAL` |
+| `"clone"`    | Duplicate          | `DISPLAYCONFIG_TOPOLOGY_CLONE`    |
+| `"extend"`   | Extend             | `DISPLAYCONFIG_TOPOLOGY_EXTEND`   |
+| `"external"` | Second screen only | `DISPLAYCONFIG_TOPOLOGY_EXTERNAL` |
+
+`<primary>` and `<secondary>` must each be one of the above 4 strings, or the program will display an error dialog and take no action. Argument strings are case-insentitive.
+
+If *DisplayToggle* is run with no additional arguments, then the primary mode defaults to `"internal"`, and the secondary mode defaults to `"extend"`. The program should always be run with 2 arguments, or none. If any other number of arguments are given, the program will display an error dialog and take no action.
+
+It is recommended to bind a specific combination of arguments to something like a Windows shortcut, batch file, or keyboard macro so the arguments will be the same each time the program is run. This will make the program act like a true *toggle*, allowing for easy single-click swapping between frequently used presentation modes.
--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
+#include <cstdlib>
+#include <cstring>
 #include <memory>
+#include <string>
 #include <stdexcept>
 #include <system_error>
 
 #define SYSTEM_ERROR(e, m) (std::system_error((e), std::system_category(), (m)))
+
+struct TopologyPair {
+	DISPLAYCONFIG_TOPOLOGY_ID primary;
+	DISPLAYCONFIG_TOPOLOGY_ID secondary;
+};
 
 DISPLAYCONFIG_TOPOLOGY_ID GetCurrentDisplayTopology()
 {
@@ -38,32 +46,69 @@ void SetCurrentDisplayTopology(DISPLAYCONFIG_TOPOLOGY_ID topology)
 	}
 }
 
-void SwapCurrentTopology()
+// The primary topology is chosen if the current topology is any other than the primary.
+// The secondary topology is chosen if the current topology is the primary.
+void SwapCurrentTopology(DISPLAYCONFIG_TOPOLOGY_ID primary, DISPLAYCONFIG_TOPOLOGY_ID secondary)
 {
-	constexpr auto INTERNAL = DISPLAYCONFIG_TOPOLOGY_INTERNAL;
-	constexpr auto EXTEND = DISPLAYCONFIG_TOPOLOGY_EXTEND;
-	DISPLAYCONFIG_TOPOLOGY_ID topology = GetCurrentDisplayTopology();
-	DISPLAYCONFIG_TOPOLOGY_ID newTopology = (topology == INTERNAL) ? EXTEND : INTERNAL;
-	SetCurrentDisplayTopology(newTopology);
+	DISPLAYCONFIG_TOPOLOGY_ID current = GetCurrentDisplayTopology();
+	SetCurrentDisplayTopology((current == primary) ? secondary : primary);
+}
+
+DISPLAYCONFIG_TOPOLOGY_ID StringToTopology(const char* topology) 
+{
+	if (!_stricmp(topology, "internal")) {
+		return DISPLAYCONFIG_TOPOLOGY_INTERNAL;
+	}
+	else if (!_stricmp(topology, "clone")) {
+		return DISPLAYCONFIG_TOPOLOGY_CLONE;
+	}
+	else if (!_stricmp(topology, "extend")) {
+		return DISPLAYCONFIG_TOPOLOGY_EXTEND;
+	}
+	else if (!_stricmp(topology, "external")) {
+		return DISPLAYCONFIG_TOPOLOGY_EXTERNAL;
+	}
+	else {
+		std::string msg("Invalid topology specified on command line: ");
+		msg += topology;
+		throw std::runtime_error(msg);
+	}
+}
+
+TopologyPair ArgsToTopologies(int argc, char** argv)
+{
+	if (argc < 2) {
+		return TopologyPair{ DISPLAYCONFIG_TOPOLOGY_INTERNAL, DISPLAYCONFIG_TOPOLOGY_EXTEND };
+	}
+	else if (argc == 3) {
+		return TopologyPair{ StringToTopology(argv[1]), StringToTopology(argv[2]) };
+	}
+	else {
+		std::string msg("Invalid usage:\n\n");
+		msg += GetCommandLineA();
+		msg += "\n\nProgram should be run with 2 arguments, or none";
+		throw std::runtime_error(msg);
+	}
 }
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance, 
 	_In_opt_ HINSTANCE hPrevInstance, 
-	_In_ LPSTR pCmdLine,
+	_In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow)
 {
+	try {
+		TopologyPair topologies = ArgsToTopologies(__argc, __argv);
+		SwapCurrentTopology(topologies.primary, topologies.secondary);
+	}
+	catch (const std::exception& e) {
+		MessageBoxA(nullptr, e.what(), "DisplayToggle error", MB_OK | MB_ICONERROR);
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+
 	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(pCmdLine);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
-
-	try {
-		SwapCurrentTopology();
-	}
-	catch (std::exception& e) {
-		MessageBoxA(nullptr, e.what(), "DisplayToggle error", MB_OK | MB_ICONERROR);
-		return 1;
-	}
-	return 0;
 }
